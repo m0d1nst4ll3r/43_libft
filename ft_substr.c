f4e66e32a23ebbaea2202db3792135e6ea817b09
Raphael Pohlen <rapohlen@f1r11s11.clusters.42paris.fr>
2025-11-08 17:44:12 +0100

    fixed substr to use sub-funcs (but there is no strndup so it's stupid)

--- ft_substr.c
+++ ft_substr.c
@@ -6,36 +6,30 @@
 /*   By: rapohlen <rapohlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 09:42:16 by rapohlen          #+#    #+#             */
-/*   Updated: 2025/11/06 12:10:18 by rapohlen         ###   ########.fr       */
+/*   Updated: 2025/11/08 17:43:12 by rapohlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(const char *s, unsigned int start, size_t len)
+static char	*substr_strndup(const char *s, size_t n)
 {
-	size_t	i;
+	size_t	len;
 	char	*new;
 
-	if (!s)
-		return (NULL);
-	i = 0;
-	while (s[i] && i < start)
-		i++;
-	if (i < start)
-		return (NULL);
-	i = 0;
-	while (s[start + i] && i < len)
-		i++;
-	new = malloc(i + 1);
+	len = ft_strlen(s);
+	if (len > n)
+		len = n;
+	new = malloc(len + 1);
 	if (!new)
 		return (new);
-	i = 0;
-	while (s[start + i] && i < len)
-	{
-		new[i] = s[start + i];
-		i++;
-	}
-	new[i] = 0;
+	ft_strlcpy(new, s, len + 1);
 	return (new);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	if (!s || ft_strlen(s) < start)
+		return (NULL);
+	return (substr_strndup(s + start, len));
 }
